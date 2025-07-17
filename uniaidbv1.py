fbd48685a92be8c23fafe330827f982b21637102
vincentvtvt <wshengv@gmail.com>
2025-07-17 09:59:07 +0800

    Update uniaidbv1.py

--- uniaidbv1.py
+++ uniaidbv1.py
@@ -4,7 +4,7 @@ import time
 import json
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, JSON, ForeignKey, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from datetime import datetime, timedelta
 import requests
